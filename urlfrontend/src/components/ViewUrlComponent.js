ea2de9f68ea09e52acce8797469b6ea92b9c38db
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2023-11-13 20:11:55 -0300

    Inicial Commit

---
+++ urlfrontend/src/components/ViewUrlComponent.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const ViewUrlComponent= () => {
+    const [urls, setUrls] = useState([]);
+
+    useEffect(() => {
+      const fetchUrlAndSetUrl = async () => {
+        const result = await axios.get("http://localhost:3333/all");
+        console.log(result.data);        
+        setUrls(result.data);
+      };
+      fetchUrlAndSetUrl();
+    }, []);
+
+    async function Teste (urlid){
+      try {
+        const result = await axios.get(`http://localhost:3333/find/${urlid}`);
+      console.log(result);
+      console.log(urlid);
+      } catch (error) {
+        console.log(error);
+      }
+      
+     
+    }
+
+  return (
+    <div>
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>Url Original</th>
+            <th>Url Encurtada</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {urls.map((url, idx) => (
+            <tr key={idx}>
+              <td>{url.origUrl}</td>
+              <td>
+                <a onClick={()=> Teste(url.urlId)} href={`${url.origUrl}`}>{url.shortUrl}</a>
+              </td>             
+              <td>{url.clicks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ViewUrlComponent;
