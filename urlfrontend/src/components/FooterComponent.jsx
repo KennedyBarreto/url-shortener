5f35e9407f5febfa637f184c8756fc2d21281eff
Kennedy Barreto <kennedygcbarreto@hotmail.com>
2023-11-17 16:38:42 -0300

    fix: fix logo size

--- urlfrontend/src/components/FooterComponent.jsx
+++ urlfrontend/src/components/FooterComponent.jsx
@@ -6,7 +6,7 @@ const FooterComponent = () => {
     <div className="imagens">
         <img src="innov.png"/>
        <a href="#" target="_blank"><img src="dpg.png"/></a> 
-       <a href="#" target="_blank"><img src="zap.png"/></a>
+       <a  href="#" target="_blank"><img  className="zap" src="zap.png"/></a>
        </div>
 </div>
 <div className="rodape"><h4>Todos os direitos reservados 2023</h4></div>
