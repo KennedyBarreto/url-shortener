82cb52bffdedb676b8da79269582c9c847bb2e06
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2023-11-28 18:03:38 -0300

    refactor: mudança temporaria na regexp

--- urlbackend/Util/util.js
+++ urlbackend/Util/util.js
@@ -1,10 +1,6 @@
 function validateUrl(value) {
-    var urlPattern = new RegExp('^(http(s?):\\/\\/)?'+ // validate protocol
-          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-          '(\\#[-a-z\\d_]*)?$','i');
+    var urlPattern = new RegExp('^(http(s?):\\/\\/)?'); // validate protocol
+          
   
         return !!urlPattern.test(value);
   }
