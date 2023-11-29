826f465c53ca1d244eadb8c84e236f1698d0846d
Kennedy Barreto <kennedygcbarreto@hotmail.com>
2023-11-29 15:39:39 -0300

    feat: alteração da regexp p/ aceitar links de wpp

--- urlbackend/Util/util.js
+++ urlbackend/Util/util.js
@@ -1,8 +1,14 @@
 function validateUrl(value) {
-    var urlPattern = new RegExp('^(http(s?):\\/\\/)?'); // validate protocol
-          
+    var urlPattern = new RegExp('^(http(s?)://)?' + // validate protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+      '(\\:\\d+)?(\/[-a-z\\d%_.~+]*)*' + // validate port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+      '(\\#[-a-z\\d_]*)?$|' + // end of the URL pattern or
+      'https?:\/\/api\.whatsapp\.com\/send.*', 'i'); // WhatsApp URL pattern
   
-        return !!urlPattern.test(value);
+    return !!urlPattern.test(value);
   }
   
   module.exports = { validateUrl };
+  
