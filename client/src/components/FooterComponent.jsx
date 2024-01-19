94482f3e953ba55fc1e01376be8d7d9f358f9d0a
Kennedy Barreto <99549329+KennedyBarreto@users.noreply.github.com>
2024-01-19 16:19:03 -0300

    feat: changed footer content

--- client/src/components/FooterComponent.jsx
+++ client/src/components/FooterComponent.jsx
@@ -1,33 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const FooterComponent = () => {
   return (
     <footer>
-      <div id="footer"> 
-
-        <div>
-          <img src="logo-innovtech.png" alt="InnovTech Logo" />
-          </div>
-
+      <div id="footer">
+        <div className="endNote">
           <div>
-          <a href="https://www.grupodpg.com.br/" target="_blank" rel="noopener noreferrer">
-            <img src="logo-dpg.png" alt="DPG Logo" />
-          </a>
+            <h4>
+              Feito com ❤️ por{" "}
+              <a
+                href="https://github.com/KennedyBarreto"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Kennedy Barreto
+              </a>
+            </h4>
           </div>
-          
-          <div className='zap'>
-          <a href="https://zapcontabil.com/" target="_blank" rel="noopener noreferrer">
-            <img src="logo-zapcontabil.png" alt="Zapcontabil Logo" />
-          </a>
-          </div>
-          </div>
-          
-      <div className="endNote">
-        <div>
-        <h4>COMHUB 2023 © Todos os direitos reservados</h4>
         </div>
       </div>
-      
     </footer>
   );
 };
